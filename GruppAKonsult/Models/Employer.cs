c7a7a9623bea323f7453c4940e546778d88f7890
Sanna Jangklint <SannaJangklint@hotmail.com>
2019-10-23 20:37:44 +0200

    Lagt till klasser för frilansare och arbetsgivare med några egenskaper

---
+++ GruppAKonsult/Models/Employer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GruppAKonsult.Models
+{
+    public class Employer
+    {
+        public int EmployerID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Company { get; set; }
+
+        public int Phonenumber { get; set;}
+        
+        public char Address { get; set;}
+        
+    }
+
+    
+}
---
+++ GruppAKonsult/Models/Freelancer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GruppAKonsult.Models
+{
+    public class Freelancer
+    {
+        public int FreelancerID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Nationality { get; set; }
+
+        public int Phonenumber { get; set; }
+
+        public char Address { get; set; }
+    }
+}
