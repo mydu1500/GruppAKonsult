9f6dbe71f71e070aeddd7cf833557043c9896c35
Sanna Jangklint <SannaJangklint@hotmail.com>
2019-10-24 20:42:44 +0200

    Lagt till en sida för inlogg med formulär

---
+++ GruppAKonsult/Controllers/LogInController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GruppAKonsult.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+    }
+}
