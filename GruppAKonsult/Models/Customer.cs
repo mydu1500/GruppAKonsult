ed6a509bc4e4fbfe2e5e58f1fd666612835b05a2
Israel Sebastian Rubio <israel.seb@hotmail.com>
2019-10-30 21:59:52 +0100

    Database Model feature

--- GruppAKonsult/Models/Customer.cs
+++ GruppAKonsult/Models/Customer.cs
@@ -12,39 +12,23 @@ namespace GruppAKonsult.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class Freelancer
+    public partial class Customer
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Freelancer()
+        public Customer()
         {
-            this.CV = new HashSet<CV>();
             this.Jobbank = new HashSet<Jobbank>();
-            this.Language = new HashSet<Language>();
             this.Profile = new HashSet<Profile>();
-            this.Skills = new HashSet<Skills>();
         }
     
-        public int Candidate_Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Nationality { get; set; }
-        public string Address { get; set; }
-        public Nullable<int> Postalnumber { get; set; }
-        public string City { get; set; }
-        public Nullable<int> Birthdate { get; set; }
-        public string Cityofbirth { get; set; }
+        public int Customer_Id { get; set; }
+        public string Companyname { get; set; }
         public string Email { get; set; }
         public Nullable<int> Phonenumber { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<CV> CV { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Jobbank> Jobbank { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Language> Language { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Profile> Profile { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Skills> Skills { get; set; }
     }
 }
