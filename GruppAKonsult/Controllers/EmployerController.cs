34a59440be47061fcc16e3d5e627e3e2b9036df5
Linh Duong <sigma_theta@hotmail.com>
2019-10-31 21:53:45 +0100

    lagt till employer

---
+++ GruppAKonsult/Controllers/EmployerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GruppAKonsult.Controllers
+{
+    public class EmployerController : Controller
+    {
+        // GET: Employer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EmployerView()
+        {
+            return View();
+        }
+
+    }
+}
