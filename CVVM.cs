deeb73c69a3d2f2042a21d6b6b2d0541065a6398
Sanna Jangklint <SannaJangklint@hotmail.com>
2019-11-05 15:50:08 +0100

    Skapat en ViewModels folder med en klass för cv. Lagt till lite kod i index m,m

---
+++ CVVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class CVVM
+{
+	public CVVM()
+	{
+	}
+
+    public CV CV { get; set; }
+    public IEnumerable<SelectListItem> AllProfessions { get; set; }
+    private List<int> listOfProfessions;
+    public List<int> ListOfProfessions
+    {
+        get
+        {
+            if (listOfProfessions == null)
+            {
+                listOfProfessions = CV.Profession.Select(m => m.Candidate_Id).ToList();
+            }
+            return listOfProfessions;
+        }
+        set { listOfProfessions = value; }
+    }
+}
