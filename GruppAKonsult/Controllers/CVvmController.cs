65e6b375bc43da4cd28dd482ff9910bac0e4d46a
Linh Duong <sigma_theta@hotmail.com>
2019-11-07 11:09:17 +0100

    Lagt till controller

---
+++ GruppAKonsult/Controllers/CVvmController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GruppAKonsult.Controllers
+{
+    public class CVvmController : Controller
+    {
+        // GET: CVvm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Skapa en lista med vymodellobjekt från våra viewmodel
+        // Listan som innehåller en massa objekt 
+    }
+}
