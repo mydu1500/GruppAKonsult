89038c4d6ee330d38bc54e6bb51e88f1671c11c5
Sanna Jangklint <SannaJangklint@hotmail.com>
2019-11-07 11:57:17 +0100

    Lagt till en lista m,m i CVvmController

--- GruppAKonsult/Controllers/CVvmController.cs
+++ GruppAKonsult/Controllers/CVvmController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GruppAKonsult.Models;
+using GruppAKonsult.ViewModels;
 
 namespace GruppAKonsult.Controllers
 {
@@ -15,5 +17,31 @@ namespace GruppAKonsult.Controllers
         }
         //Skapa en lista med vymodellobjekt från våra viewmodel
         // Listan som innehåller en massa objekt 
+
+        public List<CVViewModel> cvvmList()
+        {
+            GruppAKonsult_dbEntities2 vmdb = new GruppAKonsult_dbEntities2(); //db Context class
+            List<CVViewModel> cvVMList = new List<CVViewModel>(); //lista med detaljer fårn både Student och Class modellerna
+            var cvlist = (from cvitem in vmdb.CV
+                          join freelanceritem in vmdb.Freelancer on cvitem.CV_Id equals
+              freelanceritem.Candidate_Id
+                          select new
+                          {
+                              cvitem.Drivinglicense,
+                              cvitem.Education,
+                              cvitem.Workexperience,
+                              cvitem.Description,
+                              freelanceritem.Firstname,
+                              freelanceritem.Lastname,
+                              freelanceritem.Nationality,
+                              freelanceritem.Phonenumber,
+
+                          }).ToList();
+            ////Query för att hämta data från databasen genom att använda JOIN för att joina de två tabellerna och lagra data i studentlist
+            //    foreach (var item in studentlist) //foreach loop för att fylla data från
+            //    studentlist till List < StudentVM >
+            {
+            }
+        }
     }
 }
