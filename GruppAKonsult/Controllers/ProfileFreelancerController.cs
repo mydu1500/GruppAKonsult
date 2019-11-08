dae330819f956a67ab1dd020b2aefd8b9bc74ea4
Israel Sebastian Rubio <israel.seb@hotmail.com>
2019-11-08 01:01:09 +0100

    Profil sida

--- GruppAKonsult/Controllers/ProfileFreelancerController.cs
+++ GruppAKonsult/Controllers/ProfileFreelancerController.cs
@@ -1,27 +1,30 @@
-﻿using GruppAKonsult.Models;
-using GruppAKonsult.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GruppAKonsult.Models;
+using GruppAKonsult.ViewModels;
 
 namespace GruppAKonsult.Controllers
 {
     public class ProfileFreelancerController : Controller
     {
-
         private GruppAKonsult_dbEntities2 db = new GruppAKonsult_dbEntities2();
-        // GET: ProfileFreelancer
-        public ActionResult Index()
-        {
-            return View();
-        }
+
+        //Ta bort denna kod, kompileringsfel
+        //// GET: ProfileFreelancer
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult ProfileFreelancer()
         {
             return View();
         }
+
+
 
         public ActionResult EmptyNewProfile()
         {
@@ -30,20 +33,90 @@ namespace GruppAKonsult.Controllers
 
         public ActionResult FreelancerCV()
         {
-            var model = new FreeLancerCVViewModel();
+            var model = new FreelancerCVViewModel();
+
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult ProfileFreelancer(FreeLancerCVViewModel model)
+        public ActionResult ProfileFreelancer(FreelancerCVViewModel model)
         {
             if (ModelState.IsValid)
             {
-                db.Freelancer.Add(model.Freelancer);
+                var candidate = db.Freelancer.Add(model.Freelancer);
+
+                db.SaveChanges();
+
+                
+                model.CV.Candidate_Id = candidate.Candidate_Id;
+
+
+                //CV_ID får 0 värde och det är därför vi summerar 1 till vår query 
+                model.CV.CV_Id = db.CV.Max(x => x.CV_Id) + 1;
+
+                var cv = db.CV.Add(model.CV);
+
+                db.SaveChanges();
+
+                var languages = model.SelectedLanguage;
+
+                var l = new Language
+                {
+                    CV_Id = cv.CV_Id,
+                    Candidate_Id = candidate.Candidate_Id,
+                    Swedish = languages.Contains("2") ? "True" : "False",
+                    English = languages.Contains("3") ? "True" : "False",
+                    French = languages.Contains("4") ? "True" : "False",
+                    Spanish = languages.Contains("5") ? "True" : "False",
+                    German = languages.Contains("6") ? "True" : "False",
+                    Norwegian = languages.Contains("7") ? "True" : "False",
+                    Danish = languages.Contains("8") ? "True" : "False",
+                    Finnish = languages.Contains("9") ? "True" : "False",
+                };
+
+                db.Language.Add(l);
+
+                var professions = model.SelectedProfessions;
+
+                var p = new Profession
+                {
+                    CV_Id = cv.CV_Id,
+                    Candidate_Id = candidate.Candidate_Id,
+
+                    Programmer = professions.Contains("2") ? "True" : "False",
+                    Webbdeveloper = professions.Contains("3") ? "True" : "False",
+                    Javadeveloper = professions.Contains("4") ? "True" : "False",
+                    Scrummaster = professions.Contains("5") ? "True" : "False",
+                    Frontenddeveloper = professions.Contains("6") ? "True" : "False",
+                    Backenddeveloper = professions.Contains("7") ? "True" : "False",
+                };
+
+                db.Profession.Add(p);
+
+                var skills = model.SelectedSkills;
+
+                var s = new Skills
+                {
+                    CV_Id = cv.CV_Id,
+                    Candidate_Id = candidate.Candidate_Id,
+                    Java = skills.Contains("1") ? "True" : "False",
+                    HTML = skills.Contains("2") ? "True" : "False",
+                    JavaScript = skills.Contains("3") ? "True" : "False",
+                    CSS = skills.Contains("4") ? "True" : "False",
+                    C__ = skills.Contains("5") ? "True" : "False",
+                    JQuery = skills.Contains("6") ? "True" : "False",
+                    SQL = skills.Contains("7") ? "True" : "False",
+
+                };
+
+                db.Skills.Add(s);
+
+
                 db.SaveChanges();
 
                 //return RedirectToAction("Index");
             }
+
             return View();
         }
     }
--- GruppAKonsult/ViewModels/FreeLancerCVViewModel.cs
+++ GruppAKonsult/ViewModels/FreeLancerCVViewModel.cs
@@ -1,14 +1,80 @@
-﻿using GruppAKonsult.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Mvc;
+using GruppAKonsult.Models;
 
 namespace GruppAKonsult.ViewModels
 {
-    public class FreeLancerCVViewModel : CVViewModel
+    public class FreelancerCVViewModel : CVViewModel
     {
+
+        public FreelancerCVViewModel()
+        {
+            AddLanguageList();
+            AddProfessionList();
+            AddSkillsList();
+        }
+
         public Freelancer Freelancer { get; set; }
 
+        public CV CV { get; set; }
+
+        public string[] SelectedLanguage { get; set; }
+
+        public string[] SelectedProfessions { get; set; }
+
+        public string[] SelectedSkills { get; set; }
+
+        public IEnumerable<SelectListItem> LanguageList { get; set; }
+
+        public IEnumerable<SelectListItem> ProfessionList { get; set; }
+
+        public IEnumerable<SelectListItem> SkillsList { get; set; }
+
+        private void AddLanguageList()
+        {
+            LanguageList = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "1", Text = "Språk"},
+                new SelectListItem { Value = "2", Text = "Svenska"},
+                new SelectListItem { Value = "3", Text = "Engelska"},
+                new SelectListItem { Value = "4", Text = "Franska"},
+                new SelectListItem { Value = "5", Text = "Spanska"},
+                new SelectListItem { Value = "6", Text = "Tyska"},
+                new SelectListItem { Value = "7", Text = "Norska"},
+                new SelectListItem { Value = "8", Text = "Danska"},
+                new SelectListItem { Value = "9", Text = "Finska"}
+
+
+            };
+        }
+
+        private void AddProfessionList()
+        {
+            ProfessionList = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "1", Text = "Grafisk designer" },
+                new SelectListItem { Value = "2", Text = "Programmerare" },
+                new SelectListItem { Value = "3", Text = "Webbutvecklare" },
+                new SelectListItem { Value = "4", Text = "Javautvecklare" },
+                new SelectListItem { Value = "5", Text = "Scrummaster" },
+                new SelectListItem { Value = "6", Text = "Frontend utvecklare" },
+                new SelectListItem { Value = "7", Text = "Backend utvecklar" }
+            };
+        }
+
+        private void AddSkillsList()
+        {
+            SkillsList = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "1", Text = "Java" },
+                new SelectListItem { Value = "2", Text = "HTML" },
+                new SelectListItem { Value = "3", Text = "Javascript" },
+                new SelectListItem { Value = "4", Text = "CSS" },
+                new SelectListItem { Value = "5", Text = "C++" },
+                new SelectListItem { Value = "6", Text = "JQuery" },
+                new SelectListItem { Value = "7", Text = "SQL" }
+            };
+        }
     }
 }
