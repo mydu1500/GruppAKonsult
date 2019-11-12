a61c691cba728f8df310941e77f7da45d504effc
Israel Sebastian Rubio <israel.seb@hotmail.com>
2019-11-12 21:09:18 +0100

    ViewModel

---
+++ GruppAKonsult/ViewModels/ProfileFreelancerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GruppAKonsult.Models;
+
+namespace GruppAKonsult.ViewModels
+{
+    public class ProfileFreelancerViewModel
+    {
+        public Freelancer Freelancer { get; set; }
+
+        public  ProfilePicPath { get; set; }
+}
+}
